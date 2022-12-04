eb51b1194feaa1fe18758bf20c233033bdc52f28
Luca Georges Francois <luca@quartz.technology>
2022-12-04 13:07:21 -0800

    docs: add description for the verify cli
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- verify/configuration.go
+++ verify/configuration.go
@@ -5,11 +5,15 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Configuration holds the data transparency and beacon clients as well as the desired slot to
+// analyze.
 type Configuration struct {
 	base *common.BaseConfiguration
 	slot uint64
 }
 
+// ConfigurationFromViper creates a new Configuration using the base configuration extractor and
+// the value for the bound --slot flag and its corresponding viper key.
 func ConfigurationFromViper(v *viper.Viper) *Configuration {
 	return &Configuration{
 		base: common.BaseConfigurationFromViper(v),
--- verify/flags.go
+++ verify/flags.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Flags registers the flags for the verify command.
 func Flags(v *viper.Viper, f *pflag.FlagSet) {
 	verifySlotFlag(v, f)
 }
@@ -16,6 +17,9 @@ const (
 	slotEnv      = "CHARON_VERIFY_SLOT"
 )
 
+// verifySlotFlag defines a --slot flag and binds it to a viper key used to extract its
+// corresponding value from the .yaml or .json files. Finally, it uses the same key to bind
+// the environment variable.
 func verifySlotFlag(v *viper.Viper, f *pflag.FlagSet) {
 	// verify --slot <SLOT>
 	f.Uint64(slotFlag, 0, "The slot to verify.")
@@ -25,6 +29,7 @@ func verifySlotFlag(v *viper.Viper, f *pflag.FlagSet) {
 	cobra.CheckErr(err)
 }
 
+// GetSlotToVerify uses the bound viper key to get the value of the --slot flag.
 func GetSlotToVerify(v *viper.Viper) uint64 {
 	return v.GetUint64(slotViperKey)
 }
