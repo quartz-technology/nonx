3a9421efc6c93923480a2ecbc71cdf901187d7c0
Luca Georges Francois <luca@quartz.technology>
2022-12-04 13:02:57 -0800

    docs: add description for the watch configuration
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- watch/configuration.go
+++ watch/configuration.go
@@ -5,10 +5,13 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Configuration is used by the watch Run method and holds the common configuration only,
+// as it does not require any extra configuration to perform the analysis for now.
 type Configuration struct {
 	base *common.BaseConfiguration
 }
 
+// ConfigurationFromViper creates a new Configuration using the base configuration extractor.
 func ConfigurationFromViper(v *viper.Viper) *Configuration {
 	return &Configuration{
 		base: common.BaseConfigurationFromViper(v),
